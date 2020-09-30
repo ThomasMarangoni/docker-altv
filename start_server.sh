88dffb0e6149cdea1529d2078431c71f096e0cda
Thomas Marangoni <30683046+ThomasMarangoni@users.noreply.github.com>
2020-09-30 10:47:39 +0200

    Added node_modules copy

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,6 @@
 BASEDIR=$(dirname $0)
 export LD_LIBRARY_PATH=${BASEDIR}
 cd ${BASEDIR}
+rm -rf node_modules/
+cp -f /altv-persistend/node_modules/ node_modules/
 ./altv-server --config "config/server.cfg" --logfile "logs/server.log"
