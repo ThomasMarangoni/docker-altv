83d4db20149ac0dca483e299e627601c592bf915
Thomas Marangoni <30683046+ThomasMarangoni@users.noreply.github.com>
2020-09-30 21:53:16 +0200

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@ BASEDIR=$(dirname $0)
 export LD_LIBRARY_PATH=${BASEDIR}
 cd ${BASEDIR}
 rm -rf node_modules/
-cp -f /altv-persistend/node_modules/ node_modules/
+cp -rf /altv-persistend/node_modules/ node_modules/
 ./altv-server --config "config/server.cfg" --logfile "logs/server.log"
